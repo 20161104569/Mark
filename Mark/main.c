6739ca8b59735d45ad478a7f51124f89cd6ed924
20161104569 <2174037646@qq.com>
2018-06-27 09:37:39 +0800

    dafen
    裁判打分

--- Mark/main.c
+++ Mark/main.c
@@ -8,8 +8,23 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+void main()
+{ float score[5];
+    float min,max,ave;
+    float sum=0.0;
+    int i;
+    printf("input 5 score:\n");
+    for(i=0;i<5;i++) scanf("%f",&score[i]);
+    printf("\n");
+    min=score[0];
+    max=score[0];
+    for(i=0;i<5;i++){
+        sum=sum+score[i];
+        if(score[i]>max) max=score[i];
+        if(score[i]<min) min=score[i];
+    }
+    ave=(sum-max-min)/3;
+    printf("The max=%f\n",max);
+    printf("The min=%f\n",min);
+    printf("The ave=%f\n",ave);
 }
